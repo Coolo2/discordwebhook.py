e30e05f47c52e0035fc5876c0d93d3d0998f745b
Coolo22 <39219877+Coolo22@users.noreply.github.com>
2020-07-28 15:11:32 +0100

    Fixed for 0.0.5

--- Examples/asynchronousSENDING.py
+++ Examples/asynchronousSENDING.py
@@ -11,6 +11,6 @@ async def SendWebhook(url):
     embed.description("Simple embed description")
     embed.color(0xFF0000) # Red embed color 
 
-    await webhook.send(url, embed=embed.embed)
+    await webhook.send(url, embed=embed)
 
 #Creating an async function to send a webhook to a url. Uses the asynchronous section of the package

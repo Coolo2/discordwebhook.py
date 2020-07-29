7f0bed761f67e23d3f0ae9db88e78a31f663af36
Coolo22 <39219877+Coolo22@users.noreply.github.com>
2020-07-29 13:20:08 +0100

    Update to 0.1.1

--- discordwebhook/discordwebhook.py
+++ discordwebhook/discordwebhook.py
@@ -1,6 +1,6 @@
 class version:
     class current:
-        name = "0.0.8"
-        release_date = "28 July 2020"
+        name = "0.1.1"
+        release_date = "29 July 2020"
     
     recent = current

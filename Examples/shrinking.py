1dd31a5c4550d96f7d0d7d743e9b3368ad88ca26
Coolo22 <39219877+Coolo22@users.noreply.github.com>
2020-07-29 13:34:32 +0100

    Create shrinking.py
    Single line webhook usage

---
+++ Examples/shrinking.py
@@ -0,0 +1,4 @@
+from discordwebhook import create 
+
+create.Webhook("https://discordapp.com/api/webhooks/737632230750421023/4RH_FTtwSuIizR6TYbC3cajPkG8dr5dkdsgv4kRDGZtI_--7NLrahYezbGhQ3pAtjCzh").send(message="Hi", username="uh", tts=True)
+#Create a webhook to a url and send it with message hi and tts on. Single line.

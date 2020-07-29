ebce05286a092946a101df9d535e3cb83138f05e
Coolo22 <39219877+Coolo22@users.noreply.github.com>
2020-07-29 13:24:53 +0100

    Update for 0.1.0

--- Examples/simpleSynchronous.py
+++ Examples/simpleSynchronous.py
@@ -1,8 +1,8 @@
 from discordwebhook import create 
 
-webhook = create.Webhook()
+webhook = create.Webhook("webhook_url")
 embed = create.Embed()
-#Creating the webhook and embed objects to use
+#Creating the webhook, input a url for it to be sent and embed objects to use
 
 embed.title("Embed title")
 embed.add_field(name="Just a value title", value="Exactly the same as discord.py", inline=False)
@@ -12,5 +12,5 @@ webhook.username("Webhook username overwritten")
 webhook.message("A message to go with the embed!")
 #Setting webhook username and message (these are optional)
 
-webhook.send("webhook_url", embeds=[embed]) 
+webhook.send(embeds=[embed]) 
 #embeds can be embed without a list 
